8cc42336efbc5875431e632dc2ce7c69392841b3
ksn07 <ksn07777@gmail.com>
2018-03-30 13:54:21 +0300

    add task 11

---
+++ basic_tasks/11.py
@@ -0,0 +1,12 @@
+def decToBin(_num):
+    _bin = ""
+    while _num > 0:
+        if _num % 2 == 0:
+            _bin += "0"
+        else:
+            _bin += "1"
+        _num //= 2
+    return _bin
+
+
+print(decToBin(174))
