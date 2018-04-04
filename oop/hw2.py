b420bbd0e821466bb27b12eff670295a67759d66
ksn07 <ksn07777@gmail.com>
2018-04-04 15:43:55 +0300

    add comments to Employee class

--- oop/hw2.py
+++ oop/hw2.py
@@ -1,19 +1,24 @@
 # create a class Employee
 class Employee(object):
-    def __init__(self, fname, sname, salary, experiance):
+    def __init__(self, fname, sname, salary=500, experiance=0):
+        """Each employee must have a first and a second name.
+        The minimum salary in the company is 500$."""
         self.fname = fname
         self.sname = sname
         self.salary = salary
         self.experiance = experiance
 
     def get_salary(self):
+        """If experiance is > 2 years => salary + 200$, > 5 years => salary*1.2 + 500$"""
         if self.experiance > 2:
             return self.salary + 200
         elif self.experiance > 5:
             return self.salary*1.2 + 500
 
     def __repr__(self):
-        #return '{} {}, manager:{}, experiance: {}'.format(self.fname, self.sname, self.experiance)
+        """Representation in the form
+        '@firstName@ @secondName@, manager:@manager secondName@, experiance:@experiance@'"""
+        # return '{} {}, manager:{}, experiance: {}'.format(self.fname, self.sname, self.experiance)
         return '{} {}, experiance: {}'.format(self.fname, self.sname, self.experiance)
 
 
@@ -36,8 +41,8 @@ class Designer(Employee):
 class Manager(Employee):
     def __init__(self, fname, sname, salary, experiance, manager, members):
         Employee.__init__(self, fname, sname, salary, experiance)
-        #self.manager = manager
-        #self.members = members
+        # self.manager = manager
+        # self.members = members
 
     def get_salary(self):
         if self.members > 5:
