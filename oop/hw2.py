a601c6fee64985c5856591681b7a7b409c40d06e
anatolek <anatolek@ukr.net>
2018-04-04 20:37:25 +0300

    add "default salary" to Employee class

--- oop/hw2.py
+++ oop/hw2.py
@@ -1,5 +1,6 @@
 class Department(object):
-    def __init__(self):
+    def __init__(self, dname):
+        self.dname = dname
         self.managers = []
 
     def add_manager(self, manager):
@@ -12,7 +13,7 @@ class Department(object):
 
 
 class Employee(object):
-    def __init__(self, fname, sname, salary=500, experiance=0):
+    def __init__(self, fname, sname, salary=500., experiance=0):
         """Each employee must have a first and a second name.
         The minimum salary in the company is 500$."""
         self.fname = fname
@@ -26,6 +27,8 @@ class Employee(object):
             return self.salary + 200
         elif self.experiance > 5:
             return self.salary*1.2 + 500
+        else:
+            return self.salary
 
     def __repr__(self):
         """Representation in the form
@@ -54,7 +57,7 @@ class Manager(Employee):
             self.salary += 300
         """If more than half of team members are developers => salary*1.1"""
         # {x for i in isinstance(self.mambers(i), Developer)}
-        if len(self.members)//2 < 5: # 5 must be change later
+        if len(self.members)//2 < 5: # 5 have to be changed later
             return self.salary * 1.1
         else:
             return self.salary
@@ -77,19 +80,21 @@ class Designer(Employee):
         self.effCoeff = effCoeff
 
     def get_salary(self):
-        return Employee.get_salary(self) * self.effCoeff
+        self.test_var = Employee.get_salary(self)
+        return self.test_var * self.effCoeff
 
 
 # ------ TESTING ------ #
 des_tony = Designer('Tony', 'Gold', 500, 1, 0.9)
 dev_mark = Developer('Mark', 'Silver', 1000, 2)
-man_tina = Manager('Tina', 'Stone', 1000, 2)
+man_tina = Manager('Tina', 'Stone', 1200, 3)
 
 # add employees to manager team
 man_tina.add_member(des_tony, dev_mark)
 
 # add managers to department
-Department().add_manager(man_tina)
+ss = Department('SoftServe')
+ss.add_manager(man_tina)
 
 # employee salary
 print(des_tony.get_salary())
@@ -97,6 +102,6 @@ print(dev_mark.get_salary())
 print(man_tina.get_salary())
 
 # department give a salary to each employee
-Department().give_salary(des_tony)
-Department().give_salary(dev_mark)
-Department().give_salary(man_tina)
+ss.give_salary(des_tony)
+ss.give_salary(dev_mark)
+ss.give_salary(man_tina)
